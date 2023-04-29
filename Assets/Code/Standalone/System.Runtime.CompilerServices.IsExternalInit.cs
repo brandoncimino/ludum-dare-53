c4eee3e943ed8da27bc258c96dad73988df0b93a
Brandon Cimino <bcimino@cccis.com>
2023-04-29 01:58:00 -0500

    added a fake version of `System.Runtime.CompilerServices.IsExternalInit` so that we can use records and the `init` accessor modifier

---
+++ Assets/Code/Standalone/System.Runtime.CompilerServices.IsExternalInit.cs
@@ -0,0 +1,10 @@
+// ReSharper disable CheckNamespace
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Needed for <a href="https://docs.unity3d.com/2023.2/Documentation/Manual/CSharpCompiler.html#:~:text=compilation%20generates%20errors.-,Record%20support,-C%23%209%20init">Record support</a>
+/// </summary>
+public static class IsExternalInit
+{
+}
