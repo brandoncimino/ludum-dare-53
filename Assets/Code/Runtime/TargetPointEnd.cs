6e4cc355621235229117f300fab020b1535a7bb9
nicole <nicole.aretz@t-online.de>
2023-04-29 20:05:16 -0500

    simplified code for communication between TargetPointEnd

--- Assets/Code/Runtime/TargetPointEnd.cs
+++ Assets/Code/Runtime/TargetPointEnd.cs
@@ -6,7 +6,6 @@ public class TargetPointEnd : TargetPoint
 {
     public TargetPointEnd my_significant_other;
     public bool bool_is_start;
-    public List<TargetPointEnd> my_friends;
     
     // Start is called before the first frame update
     void Start()
@@ -32,50 +31,33 @@ public class TargetPointEnd : TargetPoint
 
     public void Find_friends()
     {
-        var center = my_body.position;
-        
-        if (bool_is_start)
-        {
-            center -= 2f * my_body.forward;
-        }
-        else
-        {
-            center += 2f * my_body.forward;
-        }
-        
+        var center = bool_is_start ? -2f * my_body.forward : 2f * my_body.forward;
+        center += my_body.position;
+
         Collider[] what_I_see = Physics.OverlapSphere(center, 2f);
-        my_friends = new List<TargetPointEnd> { };
         
         foreach (var collider in what_I_see)
         {
             var friend = collider.gameObject.GetComponent<TargetPointEnd>();
             if (friend != null)
             {
-                my_friends.Add(friend);
+                Make_friend(friend);
+                friend.Make_friend(this);
             }
         }
-
-        foreach (var friend in my_friends)
+    }
+    
+    public void Make_friend(TargetPointEnd friend)
+    {
+        if (friend == this) return;
+        
+        if (bool_is_start)
         {
-            if (friend == this) continue;
-            if (bool_is_start)
-            {
-                if (!my_previous.Contains(friend))
-                {
-                    my_previous.Add(friend);
-                }
-            }
-            else
-            {
-                if (!my_next.Contains(friend))
-                {
-                    my_next.Add(friend);
-                }   
-            }
-
-
+            if (!my_previous.Contains(friend)) my_previous.Add(friend);
+            return;
         }
         
+        if (!my_next.Contains(friend)) my_next.Add(friend);
         
     }
 }
